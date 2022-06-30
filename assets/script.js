4e5d9e16c6c47b3082391d5503544ff1c7008b6c
JioR95 <jiovani.rivera@rocketmail.com>
2022-06-30 17:16:14 -0400

    password added

--- assets/script.js
+++ assets/script.js
@@ -34,3 +34,14 @@ while (true) {
     }
 }
 
+var userWantsLowercase = window.confirm("Would you like to include lowercase letters in your password?");
+var userWantsUppercase = window.confirm("Would you like to include uppercase letters in your password?");
+var userWantsNumbers = window.confirm("Would you like to include numbers in your password?");
+var userWantsSymbols = window.confirm("Would you like to include special characters in your password?");
+
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbol = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "`", "~"];
+var userChoice = [];
+ 
