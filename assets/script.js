ad434a1a41b6d43198df0e276eb22f8d7421fd90
JioR95 <jiovani.rivera@rocketmail.com>
2022-06-30 16:19:24 -0400

    java update

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,16 @@
 // Get references to the #generate element
 var generateButton = document.querySelector("#generate");
 
-function generatePassword() 
+function randomInt(min, max){
+    if (!max) {
+        max = min 
+        min = 0
+    }
+    var rand = Math.random()
+    return Math.floor(min*(1 - rand)+ rand*max)
+}
+function getRandomitem(list){
+return list[randomInt(list.length)]
+}
+
+function generatePassword()
